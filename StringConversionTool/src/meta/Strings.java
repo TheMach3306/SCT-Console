c275ac360d353df1f45fb58f028716046dc5295b
TheMach3306 <144665069+TheMach3306@users.noreply.github.com>
2025-03-02 17:08:11 +0900

    Update Strings.java

--- StringConversionTool/src/meta/Strings.java
+++ StringConversionTool/src/meta/Strings.java
@@ -5,6 +5,7 @@ public class Strings {
 	public Strings() {}
 	
 	// A simpler version of this mouth full "System.out.println()"
+	// "NULL_STR" = Debugging string
 	public void print(String...strings) {
 		for (String string: strings) {
 			System.out.println(String.format("%s", !string.isEmpty() || !string.isBlank() ? string: "NULL_STR"));
